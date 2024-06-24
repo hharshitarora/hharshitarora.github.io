8c57ce07064af09e4a0da4d8583752ab55d86fe5
Harshit Arora <harshitarora@Harshits-MacBook-Air.local>
2024-06-23 22:44:12 -0500

    Added business card

--- react-portfolio-website-1/src/App.jsx
+++ react-portfolio-website-1/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/header/Header'
 import Nav from './components/nav/nav'
 import About from './components/about/about'
@@ -6,21 +7,31 @@ import Education from './components/education/education'
 import Experience from './components/experience/Experience'
 import Article from './components/articles/articles'
 import Contact from './components/contacts/contact'
+import Businesscard from './components/businesscard/businesscard'
 import Footer from './components/footer/footer'
 
+const MainContent = () => (
+  <>
+    <Header />
+    <Nav />
+    <About />
+    <Education />
+    <Experience />
+    <Article />
+    <Contact />
+    <Footer />
+  </>
+)
 
 export const App = () => {
   return (
-    <>
-      <Header />
-      <Nav />
-      <About />
-      <Education />
-      <Experience />
-      <Article />
-      <Contact />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/businesscard" element={<Businesscard />} />
+      </Routes>
+    </Router>
   )
 }
+
 export default App
